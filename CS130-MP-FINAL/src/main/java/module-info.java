411da0e002fa7d3ec7d5ea35831e105272a7886a
Arianne Acosta <adacosta3@up.edu.ph>
2024-04-11 22:55:44 +0800

    Reconfigured Project

---
+++ CS130-MP-FINAL/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.example.cs130mpfinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.desktop;
+
+    opens org.example.cs130mpfinal to javafx.fxml;
+    exports org.example.cs130mpfinal;
+}
---
+++ CS130-MP-FINAL/src/main/java/org/example/cs130mpfinal/MainGUI.java
@@ -0,0 +1,35 @@
+package org.example.cs130mpfinal;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainGUI extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        URL fxmlUrl = getClass().getResource("MainGUI");
+        System.out.println("URL" + fxmlUrl);
+        if (fxmlUrl == null) {
+            throw new IOException("FXML file not found");
+        }
+        Parent root = FXMLLoader.load(fxmlUrl);
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Quine-McCluskey Calculator");
+        Image appicon = new Image(getClass().getResourceAsStream("resources/placeholder.jpg"));
+        //Image appicon = new Image("/imageassets/app_icon.png");
+        stage.getIcons().add(appicon);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
